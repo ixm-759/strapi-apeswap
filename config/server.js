7a373d80eef246dcd44e5e0099ebc85ce6f4d497
Raul <raul.castro@yaydoo.com>
2021-05-03 21:59:39 -0500

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '77275e5c22f85f1abeecabbddee45862'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd02fd54a-4f1b-40f0-ba4e-4f591c01abf4'
+};
